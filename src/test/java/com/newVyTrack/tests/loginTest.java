f60c5e6c211fe7124860a63f59e7c6b01668ab21
belletrax <elifalan@yahoo.com>
2021-02-04 12:35:32 +0000

    update loginTest

--- src/test/java/com/newVyTrack/tests/loginTest.java
+++ src/test/java/com/newVyTrack/tests/loginTest.java
@@ -5,10 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.interactions.Actions;
 
-import java.util.List;
+     // Bu class'i sadece deneme amacli olusturdum
+     // Diger sayfalari olusturduktan sonra silebiliriz.
 
 public class loginTest {
     public static void main(String[] args) throws InterruptedException {
